da6290adac1dfbfd4c0f47690a79fe956bae4185
Luis López <ldotlopez@gmail.com>
2021-11-21 19:25:10 +0100

    Add copyright and minor style changes

--- dchs220/siren.py
+++ dchs220/siren.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Luis López <luis@cuarentaydos.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+
+
 import enum
 from .soapclient import SoapClient
 
@@ -12,7 +32,7 @@ class Sound(enum.Enum):
 
     @classmethod
     def fromstring(cls, s):
-        return getattr(cls, s.replace('-', '_').upper())
+        return getattr(cls, s.replace("-", "_").upper())
 
 
 class Siren(SoapClient):
@@ -41,7 +61,7 @@ class Siren(SoapClient):
             """,
             ),
         )
-        if ret != 'OK':
+        if ret != "OK":
             raise Exception(ret)
 
     def beep(self, volume=100, duration=1):
